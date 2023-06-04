f4a8ab683473171fb4d5ca7a31d997d4d4557cee
SandipNITA2025 <sarkarsandip7910@gmail.com>
2023-06-04 23:39:27 +0530

    UPADTED

--- src/pages/CountDown/CountDown.jsx
+++ src/pages/CountDown/CountDown.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-
 import { MdOutlineArrowBackIosNew } from "react-icons/md";
 import { BiSearch } from "react-icons/bi";
 import { Link } from "react-router-dom";
@@ -7,30 +6,70 @@ import axios from "axios";
 import { API } from "../../utils/API";
 
 const CountDown = () => {
-    const [details, setDetails] = useState([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        const storedPath = localStorage.getItem("path");
-        // console.log(storedPath);
-    
-        axios
-          .get(`${API}/api/mergedetails?authId=${storedPath}`)
-          .then((response) => {
-            setDetails(response.data);
-            console.log(response.data);
-          })
-          .catch((error) => {
-            console.error("Error:", error);
-          })
-          .finally(() => {
-            setLoading(false);
-          });
-      }, []);
+  const [details, setDetails] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
 
-      console.log(details);
+  useEffect(() => {
+    const storedPath = localStorage.getItem("path");
+
+    axios
+      .get(`${API}/api/mergedetails?authId=${storedPath}`)
+      .then((response) => {
+        setDetails(response.data);
+        console.log(response.data);
+        if (
+          response.data.mergedData &&
+          response.data.mergedData[0] &&
+          response.data.mergedData[0].eventDetails
+        ) {
+          const targetDate = new Date(
+            response.data.mergedData[0].eventDetails.date
+          );
+          startCountdown(targetDate);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  const startCountdown = (targetDate) => {
+    const calculateTime = () => {
+      const now = new Date().getTime();
+      const difference = targetDate - now;
+
+      const remainingDays = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const remainingHours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const remainingMinutes = Math.floor(
+        (difference % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      const remainingSeconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+      setDays(remainingDays);
+      setHours(remainingHours);
+      setMinutes(remainingMinutes);
+      setSeconds(remainingSeconds);
+    };
+
+    calculateTime();
+
+    const interval = setInterval(calculateTime, 1000);
+
+    return () => clearInterval(interval);
+  };
+
   return (
-    <div className="  w-full mx-auto mt-14 ">
-      <nav className=" flex items-center justify-between bg-white w-full p-3 shadow-md absolute top-0">
+    <div className="w-full h-full mx-auto ">
+      <nav className="flex items-center justify-between bg-white w-full p-3 shadow-md absolute top-0">
         <Link to="/">
           <MdOutlineArrowBackIosNew size={20} />
         </Link>
@@ -38,30 +77,26 @@ const CountDown = () => {
         <BiSearch size={20} />
       </nav>
 
-      {/* COUNTDOWN START*/}
-      <div className=" w-full h-full flex-col flex items-center justify-center p-2 gap-3 overflow-y-scroll">
-        <div className=" flex items-center justify-center mt-2">
+      <div className="w-full h-full flex flex-col items-center justify-center p-2 gap-3 overflow-y-scroll">
+        <div className="flex items-center justify-center mt-2">
           <img
-            className=" w-[90%]"
+            className="w-[90%]"
             src="https://i.postimg.cc/W1Sx56DM/cccc.jpg"
             alt=""
           />
         </div>
-        <div className=" text-center mt-2 space-y-2">
-          <p className=" text-[1.3rem] font-medium">
-            The best is yet to come !
-          </p>
-          <p className="text-[1.1rem] text-[#f79489] font-medium">
-            7 Days : 8 Hours
+        <div className="text-center mt-2 space-y-2">
+          <p className="text-[1.3rem] font-medium">The best is yet to come!</p>
+          <p className="text-[1rem] text-[#f79489] font-medium">
+            {days} Days : {hours} Hours : {minutes} Minutes
           </p>
         </div>
-        <div className=" mt-3">
-          <button className=" bg-gray-200 text-[1rem] text-[#f79489] p-2 px-7 rounded-3xl active:scale-95">
+        <div className="mt-3 flex items-center justify-center">
+          <button className="bg-gray-200 text-[1rem] text-[#f79489] p-2 px-7 rounded-3xl active:scale-95">
             Events
           </button>
         </div>
       </div>
-      {/* COUNTDOWN END*/}
     </div>
   );
 };
