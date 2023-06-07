9aeb207a4c28a1b2c9ac229fd17b831bdcdb1478
SandipNITA2025 <sarkarsandip7910@gmail.com>
2023-06-07 12:31:50 +0530

    Updated

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -6,11 +6,11 @@ import { Link } from "react-router-dom";
 const TopNav = ({ children, routeLink, barTitle }) => {
   return (
     <div className="relative w-full h-full m-auto">
-      <nav className="flex items-center justify-between bg-white w-full p-3 shadow-md sticky top-0 z-[5]">
+      <nav className="flex items-center justify-between bg-white w-full p-3 shadow-md sticky top-0 z-[5] sm:p-[13px] ">
         <Link to={routeLink}>
           <MdOutlineArrowBackIosNew size={20} />
         </Link>
-        <p>{barTitle}</p>
+        <p className=" text-[1.1rem]">{barTitle}</p>
         <BiSearch size={20} />
       </nav>
       {children}
